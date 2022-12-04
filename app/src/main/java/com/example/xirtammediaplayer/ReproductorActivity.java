e6cf7efd0dab02e67296305e978f25f2b801b7ff
Osaki <alejamt@gmail.com>
2022-12-04 05:54:21 +0100

    + Añadidas las canciones + Aplicación funcional en casi su totalidad

--- app/src/main/java/com/example/xirtammediaplayer/ReproductorActivity.java
+++ app/src/main/java/com/example/xirtammediaplayer/ReproductorActivity.java
@@ -2,34 +2,76 @@ package com.example.xirtammediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+
+import java.io.IOException;
 
 public class ReproductorActivity extends AppCompatActivity {
 
-    public String generos[] = {"Bandas Sonoras","Rock Alternativo","Indie"};
+    public String[] generos = {"Bandas Sonoras","Rock Alternativo","Indie"};
 
-    public String gruposBS[] = {"Matrix","Interestellar"};
-    public String gruposRA[] = {"Radiohead","Gorillaz"};
-    public String gruposI[] = {"Cariño","El Cuarteto de Nos"};
 
-    public String songsMatrix[] = {"Clubbed to Death","Rock Is Dead"};
-    public String songsInterestellar[] = {"Cornfield Chase"};
-    public String songsRadiohead[] = {"Creep"};
-    public String songsGorillaz[] = {"Feel Good Inc."};
-    public String songsCariño[] = {"Te Brillan"};
-    public String songsCuarteto[] = {"Lo malo de ser bueno"};
+    public String[] gruposBS = {"Matrix","Interestellar"};
+    public String[] gruposRA = {"Radiohead","Gorillaz"};
+    public String[] gruposI = {"Cariño","El Cuarteto de Nos"};
+
+    public String[] songsMatrix = {"Clubbed to Death","Rock Is Dead"};
+    public String[] songsInterestellar = {"Cornfield Chase"};
+    private String[] songsInterestellarSource = {"bs_cornfield_chase"};
+
+    public String[] songsRadiohead = {"Creep"};
+    private String[] songsRadioheadSource = {"ra_creep"};
+
+    public String[] songsGorillaz = {"Feel Good Inc."};
+    private String[] songsGorillazSource = {"ra_feel_good_inc"};
+
+    public String[] songsCarinio = {"Te Brillan"};
+    private String[] songsCarinioSource = {"i_te_brillan"};
+
+    public String[] songsCuarteto = {"Lo malo de ser bueno"};
+    private String[] songsCuartetoSource = {"i_lo_malo_de_ser_bueno"};
 
     public int selector;
+    public Boolean playIconActive;
+    private String source;
 
 
     public Spinner spGeneros,spGrupos, spCanciones;
     public ArrayAdapter adapterGrupos, adapterGeneros, adapterSongs;
+    public ImageButton playPauseB;
+    public MediaPlayer mediaPlayer;
 
-
+    public void setSongSource(){
+        switch (spCanciones.getSelectedItem().toString()){
+            case "Clubbed to Death":
+                source = "bs_clubbed_to_death";
+                break;
+            case "Rock Is Dead":
+                source = "bs_rock_is_dead";
+                break;
+            case "Cornfield Chase":
+                source = "bs_cornfield_chase";
+                break;
+            case "Creep":
+                source = "ra_creep";
+                break;
+            case "Feel Good Inc.":
+                source = "ra_feel_good_inc";
+                break;
+            case "Te Brillan":
+                source = "i_te_brillan";
+                break;
+            case "Lo malo de ser bueno":
+                source = "i_lo_malo_de_ser_bueno";
+                break;
+        }
+    }
 
     public void setGrupos(int i){
 
@@ -53,7 +95,6 @@ public class ReproductorActivity extends AppCompatActivity {
                 setSongsI(0);
                 break;
         }
-
     }
 
 
@@ -71,7 +112,6 @@ public class ReproductorActivity extends AppCompatActivity {
                 spCanciones.setAdapter(adapterSongs);
                 break;
         }
-
     }
 
     public void setSongsBS(int songId) {
@@ -88,14 +128,13 @@ public class ReproductorActivity extends AppCompatActivity {
                 spCanciones.setAdapter(adapterSongs);
                 break;
         }
-
     }
 
     public void setSongsI(int songId) {
 
         switch (songId){
             case 0:
-                adapterSongs = new ArrayAdapter(getApplicationContext(), R.layout.spinner_item_layout, songsCariño);
+                adapterSongs = new ArrayAdapter(getApplicationContext(), R.layout.spinner_item_layout, songsCarinio);
                 adapterSongs.setDropDownViewResource(R.layout.custom_dropdown);
                 spCanciones.setAdapter(adapterSongs);
                 break;
@@ -105,16 +144,21 @@ public class ReproductorActivity extends AppCompatActivity {
                 spCanciones.setAdapter(adapterSongs);
                 break;
         }
-
     }
 
-    private void inicializarSpinners(){
+    private void inicializarItems(){
 
         selector = 0;
+        playIconActive = true;
+        source = "bs_clubbed_to_death";
 
         spGeneros = findViewById(R.id.spinnerGeneros);
         spGrupos = findViewById(R.id.spinnerGrupos);
         spCanciones = findViewById(R.id.spinnerCanciones);
+        playPauseB = findViewById(R.id.buttonPlayPause);
+        int sound_id = getApplicationContext().getResources().getIdentifier(source, "raw",
+                getApplicationContext().getPackageName());
+        mediaPlayer = MediaPlayer.create(this, sound_id);
 
         adapterGeneros = new ArrayAdapter(this, R.layout.spinner_item_layout, generos);
         adapterGeneros.setDropDownViewResource(R.layout.custom_dropdown);
@@ -130,29 +174,52 @@ public class ReproductorActivity extends AppCompatActivity {
 
     }
 
+    public void playIcon(){
+        playPauseB.setBackgroundResource(R.drawable.play_icon);
+        playIconActive = true;
+    }
+
+    public void pauseIcon(){
+        playPauseB.setBackgroundResource(R.drawable.pause_icon);
+        playIconActive = false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reproductor);
         getSupportActionBar().hide();
 
-        inicializarSpinners();
+        inicializarItems();
 
         spGeneros.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    playIcon();
+                    mediaPlayer.stop();
+                }
+
                 setGrupos(position);
                 selector = position;
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
+                //nothing
             }
         });
 
         spGrupos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    playIcon();
+                    mediaPlayer.stop();
+                }
+
                 switch (selector){
                     case 0:
                         setSongsBS(i);
@@ -168,7 +235,45 @@ public class ReproductorActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
+                //nothing
+            }
+        });
 
+        spCanciones.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    playIcon();
+                    mediaPlayer.stop();
+                }
+
+                setSongSource();
+                int sound_id = getApplicationContext().getResources().getIdentifier(source, "raw",
+                        getApplicationContext().getPackageName());
+
+                if(sound_id != 0) {
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), sound_id);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //nothing
+            }
+        });
+
+        playPauseB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(playIconActive){
+                    pauseIcon();
+                    mediaPlayer.start();
+                } else {
+                    playIcon();
+                    mediaPlayer.pause();
+                }
             }
         });
     }
